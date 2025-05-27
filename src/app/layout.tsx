b03745c86499342377c78addd7c707a52beefedc
abhishek17551 <ababhishek113@gmail.com>
2025-05-27 13:53:31 +0530

    clerk configuration, Signup & Signin components using clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Theme from "@/components/Theme"
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,15 +26,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+   <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Theme>
-        <Header/>
-        {children}
+          <Header/>
+          {children}
         </Theme>
       </body>
     </html>
+   </ClerkProvider>
   );
 }
